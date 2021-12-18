a4770e763731b09e7ba5742ff5374bad15012251
armaBurton <armanarma@armaburton.com>
2021-12-17 16:51:01 -0800

    fixed missed rubric points

--- detail/detail.js
+++ detail/detail.js
@@ -3,16 +3,17 @@ import { renderDogDetail } from '../render-utils.js';
 
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
+window.addEventListener(`load`, async() => {
 // on load
 // get the id from URL
+    const url = new URL(location.href);
+    const search = url.searchParams;
+    const id = parseInt(search.get(`id`));
+    console.log(id);
+
+
 // use the id to fetch the dog
 // render and append this dog's details to the container
-window.addEventListener(`load`, async() => {
-    const baseUrl = document.URL;
-    const url = new URL(baseUrl);
-    const searchId = url.searchParams;
-
-    const id = searchId.get(`id`);
 
     const dog = await getDog(id);
 
